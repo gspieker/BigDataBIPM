100e28395f2d6e0f7c8fcc40371aac59bafcf783
Oybek Abdumukhtarov <129609538+oybekhwr@users.noreply.github.com>
2023-07-03 14:24:22 +0200

    Update model.py

--- finance/model.py
+++ finance/model.py
@@ -2,47 +2,29 @@ from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import Dense, LSTM
 
-def preprocess_data(data):
-    # Convert pandas DataFrame to numpy array
-    dataset = data.values
 
-    # Get the number of rows to train the model on (80% in this case)
-    training_data_len = int(np.ceil(len(dataset) * .8 ))
 
-    # Scale the data
-    scaler = MinMaxScaler(feature_range=(0,1))
-    scaled_data = scaler.fit_transform(dataset)
+def train_and_predict_model(asset, data):
+    # Preprocess data
+    training_data_len, scaled_data, scaler = preprocess_data(data['Close'])
 
-    return training_data_len, scaled_data, scaler
+    # Train model
+    model = train_model(training_data_len, scaled_data)
 
-def train_model(training_data_len, scaled_data):
-    # Create the scaled training data set
-    train_data = scaled_data[0:int(training_data_len), :]
+    # Use model to predict future prices and scale back to original data range
+    test_data = scaled_data[training_data_len - 60: , :]
+    x_test = []
+    for i in range(60, len(test_data)):
+        x_test.append(test_data[i-60:i, 0])
+    x_test = np.array(x_test)
+    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1 ))
+    predictions = model.predict(x_test)
+    predictions = scaler.inverse_transform(predictions)
 
-    # Split the data into x_train and y_train data sets
-    x_train = []
-    y_train = []
-    for i in range(60, len(train_data)):
-        x_train.append(train_data[i-60:i, 0])
-        y_train.append(train_data[i, 0])
+    # Prepare dataframe for plotting
+    train = data[:training_data_len]
+    valid = data[training_data_len:]
+    valid['Predictions'] = predictions
 
-    # Convert the x_train and y_train to numpy arrays
-    x_train, y_train = np.array(x_train), np.array(y_train)
+    return valid
 
-    # Reshape the data (LSTM expects 3D data)
-    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-
-    # Build the LSTM model
-    model = Sequential()
-    model.add(LSTM(50, return_sequences=True, input_shape= (x_train.shape[1], 1)))
-    model.add(LSTM(50, return_sequences= False))
-    model.add(Dense(25))
-    model.add(Dense(1))
-
-    # Compile the model
-    model.compile(optimizer='adam', loss='mean_squared_error')
-
-    # Train the model
-    model.fit(x_train, y_train, batch_size=1, epochs=1)
-
-    return model
