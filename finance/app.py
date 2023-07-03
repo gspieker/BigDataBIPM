f07653aaf529e8e2199ce4e13023bddef7fbb1bb
Oybek Abdumukhtarov <129609538+oybekhwr@users.noreply.github.com>
2023-07-03 14:28:42 +0200

    Update app.py

--- finance/app.py
+++ finance/app.py
@@ -92,36 +92,17 @@ def main():
     st.sidebar.info('This app is a example of')
 
 
-if __name__ == '__main__':
-    main()
-
-
-
-# Load and preprocess data
-data0 = load_quotes(asset)
-if data0 is not None:
-    data = data0.copy().dropna()
-    data.index.name = None
-
-    # Preprocess data
-    training_data_len, scaled_data, scaler = preprocess_data(data['Close'])
-
-    # Train model
-    model = train_model(training_data_len, scaled_data)
-
-    # Use model to predict future prices and scale back to original data range
-    test_data = scaled_data[training_data_len - 60: , :]
-    x_test = []
-    for i in range(60, len(test_data)):
-        x_test.append(test_data[i-60:i, 0])
-    x_test = np.array(x_test)
-    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1 ))
-    predictions = model.predict(x_test)
-    predictions = scaler.inverse_transform(predictions)
+    data0 = load_quotes(asset)
+    if data0 is not None:
+        data = data0.copy().dropna()
+        data.index.name = None
+    
+    # Preprocess and train model
+    valid = train_and_predict_model(asset, data)
 
     # Plot data
-    train = data[:training_data_len]
-    valid = data[training_data_len:]
-    valid['Predictions'] = predictions
-
     st.line_chart(valid[['Close', 'Predictions']])
+
+
+if __name__ == '__main__':
+    main()
