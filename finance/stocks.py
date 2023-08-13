719b44b0b0eaf95d08cdcfb4f01c075b08f8af97
Maximilian Sander <130981114+GITMAXIMUM@users.noreply.github.com>
2023-08-13 10:14:00 +0200

    Update stocks.py

--- finance/stocks.py
+++ finance/stocks.py
@@ -21,9 +21,12 @@ def load_data():
     return components.set_index('Symbol')
     
 # The @st.cache decorator is used to cache the output of a function. It helps improve the performance of the Streamlit app by preventing unnecessary repeated calculations when the same function is called with the same arguments.
+# load_data(): loads data about the components of the S&P 500 from a Wikipedia page and returns a DataFrame in which the symbols are set as an index. 
 
 def load_quotes(asset):
     return yf.download(asset)
+
+# Downloads historical stock data using the yfinance library for a specific stock symbol (asset).
 
 def create_model(data):
     data['Date'] = data.index
@@ -39,9 +42,13 @@ def create_model(data):
 
     return regressor, X_test, y_test
 
+# The function prepares the data for linear regression. It converts the date index into ordinal values and then performs linear regression to predict the closing prices (close) based on the data.
+
 def main():
     components = load_data()
     title = st.empty()
+
+# This is the main function of the Streamlit app. It loads the data of the S&P 500 components, initializes the Streamlit App interface and sets up the main elements of the user interface.
 
     def label(symbol):
         return symbol
@@ -49,11 +56,17 @@ def main():
     asset = st.sidebar.selectbox('Select asset', components.index.sort_values(), index=3, format_func=label)
     title.title(asset)
 
+# These lines create a Streamlit sidebar with a dropdown selection box to select an asset. Then, the title of the main interface is set to the selected asset.
+
     data0 = load_quotes(asset)
     if data0 is not None:
         data = data0.copy().dropna()
         data.index.name = None
 
+# In this section, the load_quotes() function loads historical stock data for the selected asset. Then a copy of the data is created, all rows with missing values are deleted and the index name is removed.
+
+# Visualization and interaction options:
+        
         section = st.sidebar.slider('Number of quotes', min_value=30,
                                     max_value=min([2000, data.shape[0]]),
                                     value=500, step=10)
@@ -74,8 +87,12 @@ def main():
             data[f'SMA2 {period2}'] = data['Adj Close'].rolling(period2).mean()
             data2[f'SMA2 {period2}'] = data[f'SMA2 {period2}'].reindex(data2.index)
 
+# This part of the code allows the user to interact with the application by selecting the number of quotes to display, enabling/disabling simple moving averages (SMA), and adjusting the time periods for SMA calculations.
+
         st.subheader('Chart')
         st.line_chart(data2)
+
+# This displays a line chart using the st.line_chart() function to visualize the historical closing prices and optionally the selected simple moving averages.
 
         if st.sidebar.checkbox('View statistic'):
             st.subheader('Statistic')
@@ -84,6 +101,8 @@ def main():
         if st.sidebar.checkbox('View quotes'):
             st.subheader(f'{asset} historical data')
             st.write(data2)
+
+# In this section you will find options to view statistics and historical data for the selected asset.
 
         if st.sidebar.checkbox('Predict closing price with Linear Regression'):
             model, X_test, y_test = create_model(data)
@@ -109,5 +128,11 @@ def main():
 
             st.caption("Note: This prediction is not an investment recommendation.")
 
+# In this section, users can predict closing prices using linear regression and plot the predicted versus actual closing prices on a chart. It also displays a note in the caption that the predictions of the model are not investment recommendations.
+
 if __name__ == '__main__':
     main()
+    
+# This line ensures that the main() function is executed when the script is run directly (not imported as a module).
+
+# In summary, the code creates an interactive web application that uses Streamlit to examine historical stock data, visualize price trends, calculate moving averages, and perform basic linear regression predictions. It provides a user-friendly interface for interacting with and analyzing stock data.
